5115bb886ecf9a919ff4c23af01c5f89a660ba06
Fernanda <fernanda.lima4@fatec.sp.gov.br>
2023-04-10 14:52:12 -0300

    ajuste

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { BtCol1, BtCol2, BtCol3 } from './src/Botao'
+import React from 'react'
+import { View, StyleSheet, StatusBar, SafeAreaView } from 'react-native'
+import { Comp1, Comp2, Comp3 } from './src/Botao'
 
-export default function App() {
+
+const App = () => {
   return (
-    <View style={style.App}>
-      <View style={style.Coluna}>
-        <BtCol1 />
-      </View>
-      <View style={style.Coluna}>
-        <BtCol2 />
-      </View>
-      <View style={style.Coluna}>
-        <BtCol3 />
-      </View>
-    </View>
+    <SafeAreaView style={style.app}>
+      <>
+      <View ><Comp1/></View>
+      <View ><Comp2/></View>
+      <View ><Comp3/></View>
+      </>
+
+    </SafeAreaView>
+    
+
   )
 }
+
 const style = StyleSheet.create({
-  App: {
-    flex: 1,
-    backgroundColor: "#AAAAAA",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
+
+
+  app: {
+    paddingTop: StatusBar.currentHeight,
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: '#aaaaaa',
+    flexGrow: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 155,    
   },
-  Coluna: {
-    flexDirection: "column",
-    margin: (6, 6)
-  },
-}
-)
+
+})
+
+export default App;
--- src/Botao.js
+++ src/Botao.js
@@ -1,86 +1,65 @@
 import React from 'react'
 import { Button } from 'react-native'
 
-export default Comp => {
-    function executar() {
-        console.warm('Executando!!!!')
-    }
 
-    const BtCol1 = () => {
-        return (
-            <>
-                <Button
-                    title="/"
-                    onPress={executar}
-                />
-                <Button
-                    title="7"
-                    onPress={function () {
-                        console.warn('7')
-                    }}
-                />
-                <Button
-                    title="4"
-                    onPress={() => console.warn("4")
-                    }
-                />
-                <Button
-                    title="1"
-                    onPress={() => console.warn("4")
-                    }
-                />
-            </> /*Esse é o fragmento, posso colocar vários componentes e distintos dentro dele*/
-        )
-    }
-    /*const BtCol2 = () => {
-        return (
-            <>
-                <Button
-                    title="+"
-                    onPress={executar}
-                />
-                <Button
-                    title="8"
-                    onPress={function () {
-                        console.warn('7')
-                    }}
-                />
-                <Button
-                    title="5"
-                    onPress={() => console.warn("4")
-                    }
-                />
-                <Button
-                    title="2"
-                    onPress={() => console.warn("4")
-                    }
-                />
-            </>
-        )
-    }*/
-    /*const BtCol3 = () => {
-        return (
-            <>
-                <Button
-                    title="*"
-                    onPress={executar}
-                />
-                <Button
-                    title="9"
-                    onPress={function () {
-                        console.warn('7')
-                    }}
-                />
-                <Button
-                    title="6"
-                    onPress={() => console.warn("4")
-                    }
-                />
-                <Button
-                    title="3"
-                    onPress={() => console.warn("4")
-                    }
-                />
-            </> 
-    }*/
+const Comp1 = () => {
+    return(
+        <>
+            <Button
+                title="/"
+                onPress={() => alert('Você teclou a tecla / ')} />
+            <Button
+                title="7"
+                onPress={() => alert('Você teclou a tecla 7 ')} />
+            <Button
+                title="4"
+                onPress={() => alert('Você teclou a tecla 4 ')} />
+            <Button
+                title="1"
+                onPress={() => alert('Você teclou a tecla 1 ')} />
+        </>
+    )
+    
 }
+
+const Comp2 = () => {
+    return (
+        <>        
+            <Button
+                title="+"
+                onPress={() => alert('Você teclou a tecla + ')} />
+            <Button
+                title="8"
+                onPress={() => alert('Você teclou a tecla 8 ')} />
+            <Button
+                title="5"
+                onPress={() => alert('Você teclou a tecla 5 ')} />
+            <Button
+                title="2"
+                onPress={() => alert('Você teclou a tecla 2 ')} />
+        </>
+    )
+    
+}
+
+const Comp3 = () => {
+    return(
+        <>
+            <Button
+                title="*"
+                onPress={() => alert('Você teclou a tecla * ')} />
+            <Button
+                title="9"
+                onPress={() => alert('Você teclou a tecla 9 ')} />
+            <Button
+                title="6"
+                onPress={() => alert('Você teclou a tecla 6 ')} />
+            <Button
+                title="3"
+                onPress={() => alert('Você teclou a tecla 3 ')} />
+        </>
+    )
+    
+}
+
+export {Comp1, Comp2, Comp3};
